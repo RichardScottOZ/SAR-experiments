8310e9fa355ac85fe95a08af2157c6c4ea24c6d4
RichardScottOZ <Richard.Scott@ozminerals.com>
2024-03-24 10:33:32 +1030

    total results

--- src/open-data-api.py
+++ src/open-data-api.py
@@ -7,3 +7,15 @@ query = api + 'package_search?q=cloncurry magnetotelluric&rows=1000'
 # make the get request and store it in the response object
 response = requests.get(query, headers=headers)
 
+import json
+from tqdm import tqdm
+import pandas as pd
+
+json_response = json.loads(response.text)
+
+#and get a count of results we can retrieve
+total_results = json_response['result']['count']
+print(total_results)
+
+
+
