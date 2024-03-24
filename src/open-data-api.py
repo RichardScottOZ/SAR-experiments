66ef21039aa95a11552a2d9d80eb36883c2d212a
RichardScottOZ <Richard.Scott@ozminerals.com>
2024-03-24 10:33:55 +1030

    datasets df

--- src/open-data-api.py
+++ src/open-data-api.py
@@ -17,5 +17,46 @@ json_response = json.loads(response.text)
 total_results = json_response['result']['count']
 print(total_results)
 
+getloop = int(total_results/1000) + 1
+
+if getloop > 0:
+    results_list = []
+    for i in range(getloop):
+        loopq = query + "&start=" + str(i*1000)
+        print(loopq)
+        response = requests.get(loopq, headers=headers)
+        #json_response = response.json()
+        json_response = json.loads(response.text)
+
+        results_list.append(json_response)
+
+
+
+result_ids = []
+for batch in tqdm(results_list, total=len(results_list) ):
+    results = batch['result']['results']
+    for r in results:
+        rid = r['id']
+        rauthor = r['author']
+        rspatial = r['spatial']
+        rtype = r['type']
+        
+        if len(r['resources']) > 0:
+            part_list = []
+            for part in r['resources']:
+                #modify the columns here to include what you want
+                df = pd.DataFrame([part])
+                df['rid'] = rid
+                df['rauthor'] = rauthor
+                df['rspatial'] = rspatial
+                df['type'] = rtype
+                part_list.append(df)
+                allparts = pd.concat(part_list)
+                
+            result_ids.append(allparts)
+
+dfDatasets = pd.concat(result_ids)            
+
+
 
 
